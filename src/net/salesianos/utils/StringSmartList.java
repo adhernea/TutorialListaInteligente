76fe365341e2cd9d76fcf8ab99a7e1bb594d6c4f
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-04-14 13:45:47 +0100

    feat(StringSmartList): method add String to list

--- src/net/salesianos/utils/StringSmartList.java
+++ src/net/salesianos/utils/StringSmartList.java
@@ -6,11 +6,16 @@ package net.salesianos.utils;
 public class StringSmartList {
 
   private String[] stringArray;
+  private int listIndex = 0;
 
   public StringSmartList() {
     this.stringArray = new String[1];
   }
 
-  
+  public void add(String text) {
+    this.stringArray[this.listIndex] = text;
+    this.listIndex++;
+  }
+
   
 }
