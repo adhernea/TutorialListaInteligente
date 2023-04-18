55b33f7e87baa410ea0f159e345bf80ecc7f291e
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-04-18 11:03:02 +0100

    wip: add Scanner

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 import net.salesianos.utils.StringSmartList;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
 
         StringSmartList stringList = new StringSmartList();
         stringList.add("Hola");
